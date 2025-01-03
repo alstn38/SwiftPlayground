e0b4bfbb419efdefe40ab7c141e553d71fc141f3
Minsoo Kang <alstn38@naver.com>
2025-01-03 20:34:09 +0900

    feat: Magazine Cell UI 속성 설정

--- MSTravel/MSTravel/Magazine/MagazineTableViewCell.swift
+++ MSTravel/MSTravel/Magazine/MagazineTableViewCell.swift
@@ -7,17 +7,38 @@
 
 import UIKit
 
-class MagazineTableViewCell: UITableViewCell {
-
+final class MagazineTableViewCell: UITableViewCell {
+    
+    @IBOutlet var magazineImageView: UIImageView!
+    @IBOutlet var magazineTitleLabel: UILabel!
+    @IBOutlet var magazineSubtitle: UILabel!
+    @IBOutlet var magazineDateLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        setupCell()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    func configureCell(_ magazine: Magazine) {
+        magazineTitleLabel.text = magazine.title
+        magazineSubtitle.text = magazine.subtitle
+        magazineDateLabel.text = magazine.date
     }
-
+    
+    private func setupCell() {
+        magazineImageView.contentMode = .scaleToFill
+        
+        magazineTitleLabel.font = .systemFont(ofSize: 16, weight: .bold)
+        magazineTitleLabel.textColor = .black
+        magazineTitleLabel.numberOfLines = 2
+        
+        magazineSubtitle.font = .systemFont(ofSize: 13, weight: .medium)
+        magazineSubtitle.textColor = .gray
+        magazineSubtitle.numberOfLines = 1
+        
+        magazineDateLabel.font = .systemFont(ofSize: 12, weight: .regular)
+        magazineDateLabel.textColor = .gray
+        magazineDateLabel.numberOfLines = 1
+    }
 }
