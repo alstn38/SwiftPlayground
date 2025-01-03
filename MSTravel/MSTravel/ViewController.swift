b8f6924b0b48cb2c80f3bbe7d9517c30e07f5b61
Minsoo Kang <alstn38@naver.com>
2025-01-03 19:26:43 +0900

    init - MSTravel init setting

---
+++ MSTravel/MSTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MSTravel
+//
+//  Created by 강민수 on 1/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
