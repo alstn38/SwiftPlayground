b94c6752f46663b67ed501ac66bffb2386a499f0
Minsoo Kang <alstn38@naver.com>
2025-01-05 00:30:56 +0900

    chore: 변수명 수정

--- MSTravel/MSTravel/Magazine/MagazineViewController.swift
+++ MSTravel/MSTravel/Magazine/MagazineViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class MagazineTableViewController: UITableViewController {
 
-    private var magzineArray = MagazineInfo().magazine
+    private var magazineArray = MagazineInfo().magazine
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +27,7 @@ extension MagazineTableViewController {
     
     /// TableView의 Row의 개수를 반환하는 메서드
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return magzineArray.count
+        return magazineArray.count
     }
     
     /// TableView의 Row별 Cell 반환하는 메서드
@@ -39,7 +39,7 @@ extension MagazineTableViewController {
             return UITableViewCell()
         }
         
-        let row = magzineArray[indexPath.row]
+        let row = magazineArray[indexPath.row]
         cell.configureCell(row)
         
         return cell
