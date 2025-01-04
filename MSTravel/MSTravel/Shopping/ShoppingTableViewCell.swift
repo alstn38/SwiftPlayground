7e1cada0f6f44e53cf402364962052de02a20883
Minsoo Kang <alstn38@naver.com>
2025-01-04 14:04:00 +0900

    feat: Shopping Cell UI 속성 설정 및 Data binding

---
+++ MSTravel/MSTravel/Shopping/ShoppingTableViewCell.swift
@@ -0,0 +1,48 @@
+//
+//  ShoppingTableViewCell.swift
+//  MSTravel
+//
+//  Created by 강민수 on 1/4/25.
+//
+
+import UIKit
+
+final class ShoppingTableViewCell: UITableViewCell {
+
+    @IBOutlet var shoppingBackgroundView: UIView!
+    @IBOutlet var shoppingCheckButton: UIButton!
+    @IBOutlet var shoppingListLabel: UILabel!
+    @IBOutlet var shoppingLikeButton: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setupCell()
+    }
+    
+    func configureCell(_ shoppingCategory: ShoppingCategory) {
+        shoppingListLabel.text = shoppingCategory.title
+        
+        let checkImage = shoppingCategory.isChecked
+        ? UIImage(systemName: "checkmark.square.fill")
+        : UIImage(systemName: "checkmark.square")
+        
+        let likeImage = shoppingCategory.isLiked
+        ? UIImage(systemName: "star.fill")
+        : UIImage(systemName: "star")
+        
+        shoppingCheckButton.setImage(checkImage, for: .normal)
+        shoppingLikeButton.setImage(likeImage, for: .normal)
+    }
+    
+    private func setupCell() {
+        shoppingBackgroundView.backgroundColor = .systemGray5
+        shoppingBackgroundView.layer.cornerRadius = 10
+        
+        shoppingCheckButton.tintColor = .black
+        
+        shoppingListLabel.font = .systemFont(ofSize: 12)
+        
+        shoppingLikeButton.tintColor = .black
+    }
+}
