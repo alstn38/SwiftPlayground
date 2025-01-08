0efbf2901e2cced50bd7cc2eeb9b429a7a542007
Minsoo Kang <alstn38@naver.com>
2025-01-08 20:11:28 +0900

    feat: Restaurant ViewController 속성 설정

--- MSRestaurant/MSRestaurant/Restaurant/RestaurantViewController.swift
+++ MSRestaurant/MSRestaurant/Restaurant/RestaurantViewController.swift
@@ -5,11 +5,32 @@
 //  Created by 강민수 on 1/8/25.
 //
 
+import MapKit
 import UIKit
 
-class RestaurantViewController: UIViewController {
+final class RestaurantViewController: UIViewController {
 
+    @IBOutlet private var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupNavigation()
+    }
+    
+    private func setupNavigation() {
+        let barButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "line.3.horizontal"),
+            style: .plain,
+            target: self,
+            action: #selector(filterButtonDidTap)
+        )
+        barButtonItem.tintColor = .black
+        
+        navigationItem.rightBarButtonItem = barButtonItem
+    }
+    
+    /// 필터 선택 버튼을 눌렀을 때 동작하는 메서드
+    @objc private func filterButtonDidTap(_ sender: UIBarButtonItem) {
+        
     }
 }
