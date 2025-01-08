77c7bb0ec91760b092a135172fe1f2ef18f4523d
Minsoo Kang <alstn38@naver.com>
2025-01-08 21:16:38 +0900

    feat: Restaurant filter action sheet 구현

--- MSRestaurant/MSRestaurant/Restaurant/RestaurantViewController.swift
+++ MSRestaurant/MSRestaurant/Restaurant/RestaurantViewController.swift
@@ -12,6 +12,7 @@ final class RestaurantViewController: UIViewController {
 
     @IBOutlet private var mapView: MKMapView!
     private var restaurantArray: [Restaurant] = RestaurantList().restaurantArray
+    private let foodCategoryTypeArray: [FoodCategoryType] = FoodCategoryType.allCases
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,9 +54,38 @@ final class RestaurantViewController: UIViewController {
         return annotation
     }
     
+    private func presentActionSheet() {
+        let alert = UIAlertController(
+            title: nil,
+            message: nil,
+            preferredStyle: .actionSheet
+        )
+        
+        foodCategoryTypeArray.forEach { category in
+            let action = UIAlertAction(
+                title: category.rawValue,
+                style: .default,
+                handler: { [weak self] _ in
+                    self?.filterRestaurant(category)
+                }
+            )
+            alert.addAction(action)
+        }
+        
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+    }
+    
+    /// 필터 선택시 동작하는 메서드
+    private func filterRestaurant(_ category: FoodCategoryType) {
+        print(#function, category.rawValue)
+    }
+    
     /// 필터 선택 버튼을 눌렀을 때 동작하는 메서드
     @objc private func filterButtonDidTap(_ sender: UIBarButtonItem) {
-        
+        presentActionSheet()
     }
 }
 
