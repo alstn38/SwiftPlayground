449b29ec1efebc4d72cefb502e568f5971a64751
Minsoo Kang <alstn38@naver.com>
2025-01-08 20:41:58 +0900

    feat: 전체 Annotation 표시 구현

--- MSRestaurant/MSRestaurant/Restaurant/RestaurantViewController.swift
+++ MSRestaurant/MSRestaurant/Restaurant/RestaurantViewController.swift
@@ -11,10 +11,13 @@ import UIKit
 final class RestaurantViewController: UIViewController {
 
     @IBOutlet private var mapView: MKMapView!
+    private var restaurantArray: [Restaurant] = RestaurantList().restaurantArray
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavigation()
+        setupMapView()
     }
     
     private func setupNavigation() {
@@ -29,8 +32,31 @@ final class RestaurantViewController: UIViewController {
         navigationItem.rightBarButtonItem = barButtonItem
     }
     
+    private func setupMapView() {
+        mapView.delegate = self
+        
+        let center = CLLocationCoordinate2D(latitude: 37.65430, longitude: 127.04777)
+        mapView.region = MKCoordinateRegion(center: center, latitudinalMeters: 150, longitudinalMeters: 150)
+        
+        restaurantArray.forEach { mapView.addAnnotation(makeAnnotation($0)) }
+    }
+    
+    /// Map의 Annotation을 반환하는 메서드
+    private func makeAnnotation(_ restaurant: Restaurant) -> MKPointAnnotation {
+        let annotation = MKPointAnnotation()
+        annotation.title = restaurant.name
+        annotation.coordinate = CLLocationCoordinate2D(
+            latitude: restaurant.latitude,
+            longitude: restaurant.longitude
+        )
+        
+        return annotation
+    }
+    
     /// 필터 선택 버튼을 눌렀을 때 동작하는 메서드
     @objc private func filterButtonDidTap(_ sender: UIBarButtonItem) {
         
     }
 }
+
+extension RestaurantViewController: MKMapViewDelegate { }
