7c0350917fd2d0f816b59513bf01f5c59c515272
Minsoo Kang <alstn38@naver.com>
2025-01-08 16:45:17 +0900

    Init: MSMiniGame project 생성

---
+++ MSMiniGame/MSMiniGame/Game/GameViewController.swift
@@ -0,0 +1,16 @@
+//
+//  GameViewController.swift
+//  MSMiniGame
+//
+//  Created by 강민수 on 1/8/25.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
