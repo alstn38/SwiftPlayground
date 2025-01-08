174e59b14dba2a36af3e999da55802f83bb6486e
Minsoo Kang <alstn38@naver.com>
2025-01-08 19:53:39 +0900

    init: Restaurant project 생성

---
+++ MSRestaurant/MSRestaurant/Restaurant/RestaurantViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  MSRestaurant
+//
+//  Created by 강민수 on 1/8/25.
+//
+
+import UIKit
+
+class RestaurantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
