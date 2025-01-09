a78db5bc1321a7fcddc581befa5c1542688d233b
Minsoo Kang <alstn38@naver.com>
2025-01-09 17:01:12 +0900

    init: UpDownGame project 생성

---
+++ MSUpDownGame/MSUpDownGame/Game/GameViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GameViewController.swift
+//  MSUpDownGame
+//
+//  Created by 강민수 on 1/9/25.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
