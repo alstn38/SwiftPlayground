33d6d71b416cf6185a2c2a34b26fe9f2e172b1ab
Minsoo Kang <alstn38@naver.com>
2025-01-09 21:05:14 +0900

    refactor: UpDown Game Manager 결과 반환 타입 수정

--- MSUpDownGame/MSUpDownGame/Game/GameManager.swift
+++ MSUpDownGame/MSUpDownGame/Game/GameManager.swift
@@ -10,7 +10,7 @@ import Foundation
 protocol GameManagerProtocol {
     var remainingNumberArray: [Int] { get }
     
-    func isCollectNumber(_ number: Int) -> Bool
+    func isCollectNumber(_ number: Int) -> GameStatus
 }
 
 final class GameManager: GameManagerProtocol {
@@ -22,23 +22,30 @@ final class GameManager: GameManagerProtocol {
     
     private let allNumberArray: [Int]
     private let answerNumber: Int
+    private var gameStatus: GameStatus
     private var down: Int = 1
     private var up: Int
     
     init(maxNumber: Int) {
         self.allNumberArray = Array(0...maxNumber)
         self.answerNumber = allNumberArray.randomElement() ?? 1
+        self.gameStatus = GameStatus(tryCount: 0, upDownStatus: .correct)
         self.up = maxNumber
     }
     
     /// 해당 숫자가 정답이 맞는지 결과를 확인하는 메서드
-    func isCollectNumber(_ selectNumber: Int) -> Bool {
+    func isCollectNumber(_ selectNumber: Int) -> GameStatus {
         if selectNumber == answerNumber {
-            return true
-        } else {
+            gameStatus.changeStatus(.correct)
+        } else if selectNumber > answerNumber {
+            gameStatus.changeStatus(.down)
             changeUpDownNumber(selectNumber)
-            return false
+        } else {
+            gameStatus.changeStatus(.up)
+            changeUpDownNumber(selectNumber)
         }
+        
+        return gameStatus
     }
     
     /// 선택된 번호에 맞게 정답 범위를 수정하는 메서드
