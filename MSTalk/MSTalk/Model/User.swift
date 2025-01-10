76da02d89eacbd81f523718b351387e03d773936
Minsoo Kang <alstn38@naver.com>
2025-01-10 19:20:10 +0900

    feat: User Model 생성

---
+++ MSTalk/MSTalk/Model/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  MSTalk
+//
+//  Created by 강민수 on 1/10/25.
+//
+
+import Foundation
+
+enum User: String {
+    case hue = "Hue"
+    case jack = "Jack"
+    case bran = "Bran"
+    case den = "Den"
+    case user //본인
+    case other_friend = "내옆자리의앞자리에개발잘하는친구"
+    case simsim = "심심이"
+    
+    var profileImage: String {
+        return rawValue
+    }
+}
