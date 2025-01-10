4ea508b94931d4e710c2d9c169724fc69c115a8e
Minsoo Kang <alstn38@naver.com>
2025-01-10 16:15:10 +0900

    init: MSTalk Project 생성

---
+++ MSTalk/MSTalk/Home/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MSTalk
+//
+//  Created by 강민수 on 1/10/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
