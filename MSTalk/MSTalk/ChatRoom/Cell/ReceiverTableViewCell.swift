caf65ed5d2dbb189d6e6491f3e756f2ac02cc295
Minsoo Kang <alstn38@naver.com>
2025-01-11 10:40:30 +0900

    feat: Receiver Chat Cell 속성 설정

---
+++ MSTalk/MSTalk/ChatRoom/Cell/ReceiverTableViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  ReceiverTableViewCell.swift
+//  MSTalk
+//
+//  Created by 강민수 on 1/10/25.
+//
+
+import UIKit
+
+final class ReceiverTableViewCell: UITableViewCell {
+    
+    @IBOutlet private var profileImageView: UIImageView!
+    @IBOutlet private var chatBackgroundView: UIView!
+    @IBOutlet private var chatMessageLabel: UILabel!
+    @IBOutlet private var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setupCell()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        profileImageView.layer.cornerRadius = profileImageView.frame.height / 2
+    }
+
+    private func setupCell() {
+        profileImageView.contentMode = .scaleAspectFit
+        profileImageView.tintColor = .black
+        
+        chatBackgroundView.backgroundColor = .white
+        chatBackgroundView.layer.cornerRadius = 10
+        chatBackgroundView.layer.borderWidth = 1
+        chatBackgroundView.layer.borderColor = UIColor.black.cgColor
+        
+        chatMessageLabel.font = .systemFont(ofSize: 12, weight: .regular)
+        chatMessageLabel.textColor = .black
+        chatMessageLabel.numberOfLines = 0
+        
+        dateLabel.font = .systemFont(ofSize: 11, weight: .regular)
+        dateLabel.textColor = .lightGray
+        dateLabel.numberOfLines = 1
+    }
+}
