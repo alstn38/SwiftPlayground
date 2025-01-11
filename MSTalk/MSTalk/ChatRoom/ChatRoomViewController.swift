3084d4f87b139bc77a30343193ca4ebada04a8d1
Minsoo Kang <alstn38@naver.com>
2025-01-11 12:02:28 +0900

    feat: 유저 정보에 따른 Chat Cell 구현

--- MSTalk/MSTalk/ChatRoom/ChatRoomViewController.swift
+++ MSTalk/MSTalk/ChatRoom/ChatRoomViewController.swift
@@ -9,23 +9,35 @@ import UIKit
 
 final class ChatRoomViewController: UIViewController {
 
+    var chatRoom: ChatRoom?
     @IBOutlet private var chatTableView: UITableView!
     @IBOutlet private var textViewBackgroundView: UIView!
     @IBOutlet private var textViewPlaceholderLabel: UILabel!
     @IBOutlet private var chatTextView: UITextView!
     @IBOutlet private var chatSendButton: UIButton!
-    var chatRoomName: String?
+    private var chatArray: [Chat] = [] {
+        didSet {
+            chatTableView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupNavigation()
         setupView()
+        configureView()
+        setupTableView()
     }
     
     private func setupNavigation() {
-        guard let chatRoomName else { return }
-        navigationItem.title = chatRoomName
+        guard let chatRoom else { return }
+        navigationItem.title = chatRoom.chatroomName
+    }
+    
+    private func configureView() {
+        guard let chatRoom else { return }
+        chatArray = chatRoom.chatList
     }
     
     private func setupView() {
@@ -46,6 +58,42 @@ final class ChatRoomViewController: UIViewController {
         
         chatSendButton.setImage(UIImage(systemName: "paperplane"), for: .normal)
         chatSendButton.tintColor = .lightGray
+    }
+    
+    private func setupTableView() {
+        chatTableView.delegate = self
+        chatTableView.dataSource = self
+        chatTableView.separatorStyle = .none
+        chatTableView.showsVerticalScrollIndicator = false
+        
+        let senderTableViewCellNib = UINib(nibName: SenderTableViewCell.identifier, bundle: nil)
+        chatTableView.register(senderTableViewCellNib, forCellReuseIdentifier: SenderTableViewCell.identifier)
+        
+        let receiverTableViewCellNib = UINib(nibName: ReceiverTableViewCell.identifier, bundle: nil)
+        chatTableView.register(receiverTableViewCellNib, forCellReuseIdentifier: ReceiverTableViewCell.identifier)
+    }
+}
+
+// MARK: - UITableViewDelegate, UITableViewDataSource
+extension ChatRoomViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return chatArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let chatMessage = chatArray[indexPath.row]
+        let isSenderChat: Bool = chatMessage.user == .user
+        let identifier: String = isSenderChat ? SenderTableViewCell.identifier : ReceiverTableViewCell.identifier
+        
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: identifier,
+            for: indexPath
+        ) as? ChatCellConfigurable else { return UITableViewCell() }
+        
+        cell.configureChatCell(chatMessage)
+        
+        return cell
     }
 }
 
