6fde6c42d56fd4320dd24c10fc8ff13a7536154c
Minsoo Kang <alstn38@naver.com>
2025-01-11 10:40:47 +0900

    feat: Sender Chat Cell 속성 설정

---
+++ MSTalk/MSTalk/ChatRoom/Cell/SenderTableViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  SenderTableViewCell.swift
+//  MSTalk
+//
+//  Created by 강민수 on 1/10/25.
+//
+
+import UIKit
+
+final class SenderTableViewCell: UITableViewCell {
+    
+    @IBOutlet private var chatBackgroundView: UIView!
+    @IBOutlet private var chatMessageLabel: UILabel!
+    @IBOutlet private var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        setupCell()
+    }
+
+    private func setupCell() {
+        chatBackgroundView.backgroundColor = .white
+        chatBackgroundView.layer.cornerRadius = 10
+        chatBackgroundView.layer.borderWidth = 1
+        chatBackgroundView.layer.borderColor = UIColor.black.cgColor
+        
+        chatMessageLabel.font = .systemFont(ofSize: 12, weight: .regular)
+        chatMessageLabel.textColor = .black
+        chatMessageLabel.numberOfLines = 0
+        
+        dateLabel.font = .systemFont(ofSize: 11, weight: .regular)
+        dateLabel.textColor = .lightGray
+        dateLabel.numberOfLines = 1
+    }
+}
