95581a2dfc6e50f16e9e775f3cc53759102f73fa
Minsoo Kang <alstn38@naver.com>
2025-01-13 21:13:40 +0900

    chore: TabBarController - 실험

--- MSMinFlix/MSMinFlix/Present/TabBarController/MSTabBarController.swift
+++ MSMinFlix/MSMinFlix/Present/TabBarController/MSTabBarController.swift
@@ -14,6 +14,19 @@ final class MSTabBarController: UITabBarController {
         
         setupTabBar()
         setupViewControllers()
+        
+        // addSubView 시도해보기
+//        let view = UIView()
+//        view.backgroundColor = .green
+//        view.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+//        view.addSubview(view)
+        
+        // 이유 생각 -
+        // 컨테이너는 말 그대로 담는 것임.
+        // 공식문서에서도 탭 바 컨트롤러의 뷰는 탭 바 뷰의 컨테이너일 뿐이라고 나와있음.
+        // 결론: 단순히 view는 컨테이너의 역할을 하기 때문에 addSubView를 하는 것은 불가능할 것이다.
+        
+        // 직접 Container VC를 만들때 addSubView를 한다면, 부모 - 자식 관계 형성 후, 자식의 view만 넣을 수 있는것으로 보인다.
     }
     
     private func setupTabBar() {
