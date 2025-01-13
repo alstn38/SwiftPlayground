5a8724f835f9e79a181e0c98a107bc4ca14fc2ba
Minsoo Kang <alstn38@naver.com>
2025-01-13 16:18:24 +0900

    init: MSMinFlix project 생성

---
+++ MSMinFlix/MSMinFlix/Present/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  MSMinFlix
+//
+//  Created by 강민수 on 1/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
