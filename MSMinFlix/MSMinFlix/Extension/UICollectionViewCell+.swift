6671342b29704e4a0becc8c3c70fca072c2af0ef
Minsoo Kang <alstn38@naver.com>
2025-01-13 20:59:50 +0900

    feat: CollectionView Identifier 설정

---
+++ MSMinFlix/MSMinFlix/Extension/UICollectionViewCell+.swift
@@ -0,0 +1,15 @@
+//
+//  UICollectionViewCell+.swift
+//  MSMinFlix
+//
+//  Created by 강민수 on 1/13/25.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
