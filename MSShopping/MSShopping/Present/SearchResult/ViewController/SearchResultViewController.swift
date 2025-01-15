c9e46fefaf681223965f2937f20c978b00ff23a5
Minsoo Kang <alstn38@naver.com>
2025-01-15 18:38:51 +0900

    feat: SearchResult View 속성 및 Layout 설정

---
+++ MSShopping/MSShopping/Present/SearchResult/ViewController/SearchResultViewController.swift
@@ -0,0 +1,118 @@
+//
+//  SearchResultViewController.swift
+//  MSShopping
+//
+//  Created by 강민수 on 1/15/25.
+//
+
+import SnapKit
+import UIKit
+
+final class SearchResultViewController: UIViewController {
+    
+    private var selectedFilterButtonType: FilterButton.FilterButtonType = .accuracy
+    
+    private let searchTotalCountLabel: UILabel = {
+        let label = UILabel()
+        label.text = "13,235,449개의 검색 결과" // TODO: 서버 연결시 제거
+        label.textColor = .systemGreen
+        label.font = .systemFont(ofSize: 12, weight: .bold)
+        return label
+    }()
+    
+    private let filterStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 10
+        stackView.alignment = .leading
+        return stackView
+    }()
+    
+    private let filterButtonArray: [FilterButton] = {
+        let button = FilterButton.FilterButtonType.allCases.map { FilterButton(type: $0) }
+        button[FilterButton.FilterButtonType.accuracy.rawValue].isSelected = true
+        return button
+    }()
+    
+    private let productCollectionView: UICollectionView = {
+        let screenWidth = UIScreen.main.bounds.width
+        let spacing: CGFloat = 20
+        let cellWidth = (screenWidth - spacing * 3) / 2
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = spacing
+        layout.minimumInteritemSpacing = spacing
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
+        layout.itemSize = CGSize(width: cellWidth, height: cellWidth + 50)
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .black
+        return collectionView
+    }()
+    
+    init(searchedText: String) {
+        super.init(nibName: nil, bundle: nil)
+        navigationItem.title = searchedText
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    
+    private func setupView() {
+        view.backgroundColor = .black
+        
+        setupHierarchy()
+        setupLayout()
+        setupCollectionView()
+    }
+    
+    private func setupHierarchy() {
+        [searchTotalCountLabel, filterStackView, productCollectionView].forEach {
+            view.addSubview($0)
+        }
+        
+        filterButtonArray.forEach {
+            filterStackView.addArrangedSubview($0)
+        }
+    }
+    
+    private func setupLayout() {
+        searchTotalCountLabel.snp.makeConstraints {
+            $0.top.leading.equalTo(view.safeAreaLayoutGuide).offset(10)
+        }
+        
+        filterStackView.snp.makeConstraints {
+            $0.top.equalTo(searchTotalCountLabel.snp.bottom).offset(10)
+            $0.leading.equalTo(view.safeAreaLayoutGuide).offset(10)
+        }
+        
+        productCollectionView.snp.makeConstraints {
+            $0.top.equalTo(filterStackView.snp.bottom).offset(5)
+            $0.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    private func setupCollectionView() {
+//        productCollectionView.delegate = self
+//        productCollectionView.dataSource = self
+    }
+}
+
+// MARK: - UICollectionViewDelegate, UICollectionViewDataSource
+//extension SearchResultViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+//    
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 20
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        return UICollectionViewCell()
+//    }
+//}
