bd7007779e0b94dc82a77f1bc5acf2cfdf19674e
Minsoo Kang <alstn38@naver.com>
2025-01-15 19:54:34 +0900

    feat: UICollectionViewCell Identifier 추가

---
+++ MSShopping/MSShopping/Extension/UICollectionViewCell+.swift
@@ -0,0 +1,15 @@
+//
+//  UICollectionViewCell+.swift
+//  MSShopping
+//
+//  Created by 강민수 on 1/15/25.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
