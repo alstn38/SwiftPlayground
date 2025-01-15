b5ea4b9f53e18e8921000517a394b9b809a3da7c
Minsoo Kang <alstn38@naver.com>
2025-01-15 16:00:24 +0900

    init: MS Shopping project 생성

---
+++ MSShopping/MSShopping/App/SceneDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  SceneDelegate.swift
+//  MSShopping
+//
+//  Created by 강민수 on 1/15/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ MSShopping/MSShopping/Present/Search/ViewController/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  MSShopping
+//
+//  Created by 강민수 on 1/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
