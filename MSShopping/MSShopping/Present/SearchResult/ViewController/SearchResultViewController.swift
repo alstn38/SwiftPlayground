1f689e96e543300cc4a17413aa632a2d5794442a
Minsoo Kang <alstn38@naver.com>
2025-01-16 18:12:50 +0900

    feat: 검색결과 화면 View 분리

--- MSShopping/MSShopping/Present/SearchResult/ViewController/SearchResultViewController.swift
+++ MSShopping/MSShopping/Present/SearchResult/ViewController/SearchResultViewController.swift
@@ -6,72 +6,20 @@
 //
 
 import Alamofire
-import SnapKit
 import UIKit
 
 final class SearchResultViewController: UIViewController {
     
+    private let searchResultView = SearchResultView()
     private var selectedFilterButtonType: FilterButton.FilterButtonType = .accuracy
     private var startPage: Int = 1
     private var searchedText: String
     private var productTotalCount: Int?
     private var productItemArray: [Item] = [] {
         didSet {
-            productCollectionView.reloadData()
+            searchResultView.productCollectionView.reloadData()
         }
     }
-    
-    private let searchTotalCountLabel: UILabel = {
-        let label = UILabel()
-        label.textColor = .systemGreen
-        label.font = .systemFont(ofSize: 12, weight: .bold)
-        return label
-    }()
-    
-    private let filterStackView: UIStackView = {
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.distribution = .equalSpacing
-        stackView.spacing = 10
-        stackView.alignment = .leading
-        return stackView
-    }()
-    
-    private let filterButtonArray: [FilterButton] = {
-        let button = FilterButton.FilterButtonType.allCases.map {
-            let button = FilterButton(type: $0)
-            button.tag = $0.rawValue
-            return button
-        }
-        
-        button[FilterButton.FilterButtonType.accuracy.rawValue].isSelected = true
-        return button
-    }()
-    
-    private let productCollectionView: UICollectionView = {
-        let screenWidth = UIScreen.main.bounds.width
-        let spacing: CGFloat = 20
-        let cellWidth = (screenWidth - spacing * 3) / 2
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        layout.minimumLineSpacing = 30
-        layout.minimumInteritemSpacing = spacing
-        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing, bottom: spacing, right: spacing)
-        layout.itemSize = CGSize(width: cellWidth, height: cellWidth + 60)
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.backgroundColor = .black
-        collectionView.showsVerticalScrollIndicator = true
-        collectionView.indicatorStyle = .white
-        return collectionView
-    }()
-    
-    private let indicatorView: UIActivityIndicatorView = {
-        let indicatorView = UIActivityIndicatorView()
-        indicatorView.style = .medium
-        indicatorView.color = .white
-        indicatorView.hidesWhenStopped = true
-        return indicatorView
-    }()
     
     init(searchedText: String) {
         self.searchedText = searchedText
@@ -84,76 +32,45 @@ final class SearchResultViewController: UIViewController {
         super.init(coder: coder)
     }
     
+    override func loadView() {
+        view = searchResultView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupView()
+        addTargetButton()
+        setupCollectionView()
         getProductResult()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        indicatorView.startAnimating()
+        searchResultView.indicatorView.startAnimating()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        indicatorView.stopAnimating()
-    }
-    
-    private func setupView() {
-        view.backgroundColor = .black
-        
-        setupHierarchy()
-        setupLayout()
-        setupCollectionView()
-        addTargetButton()
-    }
-    
-    private func setupHierarchy() {
-        [searchTotalCountLabel, filterStackView, productCollectionView, indicatorView].forEach {
-            view.addSubview($0)
-        }
-        
-        filterButtonArray.forEach {
-            filterStackView.addArrangedSubview($0)
-        }
-    }
-    
-    private func setupLayout() {
-        searchTotalCountLabel.snp.makeConstraints {
-            $0.top.leading.equalTo(view.safeAreaLayoutGuide).offset(10)
-        }
-        
-        filterStackView.snp.makeConstraints {
-            $0.top.equalTo(searchTotalCountLabel.snp.bottom).offset(10)
-            $0.leading.equalTo(view.safeAreaLayoutGuide).offset(10)
-        }
-        
-        productCollectionView.snp.makeConstraints {
-            $0.top.equalTo(filterStackView.snp.bottom).offset(5)
-            $0.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
-        }
-        
-        indicatorView.snp.makeConstraints {
-            $0.center.equalTo(productCollectionView)
-        }
+        searchResultView.indicatorView.stopAnimating()
     }
     
     private func addTargetButton() {
-        filterButtonArray.forEach {
+        searchResultView.filterButtonArray.forEach {
             $0.addTarget(self, action: #selector(filterButtonDidTap), for: .touchUpInside)
         }
     }
     
     private func setupCollectionView() {
-        productCollectionView.delegate = self
-        productCollectionView.dataSource = self
-        productCollectionView.register(ProductCollectionViewCell.self, forCellWithReuseIdentifier: ProductCollectionViewCell.identifier)
+        searchResultView.productCollectionView.delegate = self
+        searchResultView.productCollectionView.dataSource = self
+        searchResultView.productCollectionView.register(
+            ProductCollectionViewCell.self,
+            forCellWithReuseIdentifier: ProductCollectionViewCell.identifier
+        )
     }
     
     private func getProductResult() {
-        indicatorView.startAnimating()
+        searchResultView.indicatorView.startAnimating()
         let encodeString = searchedText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
         let url = Bundle.main.mainURL + "?query=\(encodeString)&display=30&sort=\(selectedFilterButtonType.query)&start=\(startPage)"
         let header: HTTPHeaders = [
@@ -166,9 +83,9 @@ final class SearchResultViewController: UIViewController {
             switch response.result {
             case .success(let value):
                 self?.productTotalCount = value.total
-                self?.searchTotalCountLabel.text = "\(value.total.formatted())개의 검색 결과"
+                self?.searchResultView.searchTotalCountLabel.text = "\(value.total.formatted())개의 검색 결과"
                 self?.productItemArray.append(contentsOf: value.items)
-                self?.indicatorView.stopAnimating()
+                self?.searchResultView.indicatorView.stopAnimating()
             case .failure(let error):
                 print(error.localizedDescription)
             }
@@ -176,11 +93,11 @@ final class SearchResultViewController: UIViewController {
     }
     
     @objc private func filterButtonDidTap(_ sender: UIButton) {
-        productCollectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .top, animated: false)
+        searchResultView.productCollectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .top, animated: false)
         productItemArray = []
         startPage = 1
         selectedFilterButtonType = FilterButton.FilterButtonType(rawValue: sender.tag) ?? .accuracy
-        filterButtonArray.forEach { $0.isSelected = false }
+        searchResultView.filterButtonArray.forEach { $0.isSelected = false }
         sender.isSelected = true
         getProductResult()
     }
