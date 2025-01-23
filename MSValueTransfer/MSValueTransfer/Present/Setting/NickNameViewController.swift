a1de3a9332f03ed7996c420928dbad94c301673c
Minsoo Kang <alstn38@naver.com>
2025-01-23 19:40:41 +0900

    feat: NickNameViewController View 속성, Layout 설정

---
+++ MSValueTransfer/MSValueTransfer/Present/Setting/NickNameViewController.swift
@@ -0,0 +1,69 @@
+//
+//  NickNameViewController.swift
+//  MSValueTransfer
+//
+//  Created by 강민수 on 1/23/25.
+//
+
+import SnapKit
+import UIKit
+
+final class NickNameViewController: UIViewController {
+    
+    private let nickNameTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "닉네임을 입력해주세요."
+        textField.textColor = .black
+        textField.borderStyle = .roundedRect
+        return textField
+    }()
+    
+    private lazy var confirmButton: UIButton = {
+        var titleContainer = AttributeContainer()
+        titleContainer.font = UIFont.boldSystemFont(ofSize: 20)
+        
+        var configuration = UIButton.Configuration.filled()
+        configuration.attributedTitle = AttributedString("확인", attributes: titleContainer)
+        
+        let button = UIButton(configuration: configuration)
+        button.addTarget(self, action: #selector(confirmButtonDidTap), for: .touchUpInside)
+        
+        return button
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureView()
+        configureHierarchy()
+        configureLayout()
+    }
+    
+    private func configureView() {
+        view.backgroundColor = .white
+    }
+    
+    private func configureHierarchy() {
+        view.addSubviews(
+            nickNameTextField,
+            confirmButton
+        )
+    }
+    
+    private func configureLayout() {
+        nickNameTextField.snp.makeConstraints {
+            $0.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(30)
+            $0.height.equalTo(34)
+        }
+        
+        confirmButton.snp.makeConstraints {
+            $0.bottom.equalTo(view.keyboardLayoutGuide).inset(30)
+            $0.height.equalTo(60)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(40)
+        }
+    }
+    
+    @objc private func confirmButtonDidTap(_ sender: UIButton) {
+        print(#function)
+    }
+}
