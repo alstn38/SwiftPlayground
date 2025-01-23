308e40c549e63d5df01e3ed02c107f57f2ab277e
Minsoo Kang <alstn38@naver.com>
2025-01-23 18:31:25 +0900

    feat: ProfileViewController 탈퇴하기 버튼 기능 추가

--- MSValueTransfer/MSValueTransfer/Present/ProfileViewController.swift
+++ MSValueTransfer/MSValueTransfer/Present/ProfileViewController.swift
@@ -10,17 +10,36 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
+    private var isNickNameEnter: Bool = false
+    private var isBirthdayEnter: Bool = false
+    private var isLevelEnter: Bool = false
     private let nickNameLabel = TapAnimationLabel(text: "닉네임")
     private let birthdayLabel = TapAnimationLabel(text: "생일")
     private let levelLabel = TapAnimationLabel(text: "레벨")
     private let saveButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("저장하기", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.titleLabel?.font = .systemFont(ofSize: 34, weight: .bold)
-        button.layer.borderWidth = 2
-        button.layer.borderColor = UIColor.black.cgColor
-        button.layer.cornerRadius = 20
+        var titleContainer = AttributeContainer()
+        titleContainer.font = UIFont.boldSystemFont(ofSize: 20)
+        
+        var configuration = UIButton.Configuration.filled()
+        configuration.attributedTitle = AttributedString("저장하기", attributes: titleContainer)
+        
+        let button = UIButton(configuration: configuration)
+        button.isEnabled = false
+        
+        return button
+    }()
+    private lazy var withdrawButton: UIButton = {
+        var titleContainer = AttributeContainer()
+        titleContainer.font = UIFont.boldSystemFont(ofSize: 20)
+        
+        var configuration = UIButton.Configuration.filled()
+        configuration.attributedTitle = AttributedString("탈퇴하기", attributes: titleContainer)
+        configuration.baseBackgroundColor = .red
+        configuration.baseForegroundColor = .white
+        
+        let button = UIButton(configuration: configuration)
+        button.addTarget(self, action: #selector(withdrawButtonDidTap), for: .touchUpInside)
+        
         return button
     }()
 
@@ -41,30 +60,51 @@ final class ProfileViewController: UIViewController {
             nickNameLabel,
             birthdayLabel,
             levelLabel,
-            saveButton
+            saveButton,
+            withdrawButton
         )
     }
     
     private func configureLayout() {
         nickNameLabel.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide).offset(60)
+            $0.height.equalTo(60)
             $0.centerX.equalToSuperview()
         }
         
         birthdayLabel.snp.makeConstraints {
             $0.top.equalTo(nickNameLabel.snp.bottom).offset(30)
+            $0.height.equalTo(60)
             $0.centerX.equalToSuperview()
         }
         
         levelLabel.snp.makeConstraints {
             $0.top.equalTo(birthdayLabel.snp.bottom).offset(30)
+            $0.height.equalTo(60)
             $0.centerX.equalToSuperview()
         }
         
         saveButton.snp.makeConstraints {
+            $0.bottom.equalTo(withdrawButton.snp.top).offset(-30)
+            $0.height.equalTo(60)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(40)
+        }
+        
+        withdrawButton.snp.makeConstraints {
             $0.bottom.equalTo(view.keyboardLayoutGuide).inset(30)
             $0.height.equalTo(60)
             $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(40)
         }
+    }
+    
+    @objc private func withdrawButtonDidTap(_ sender: UIButton) {
+        guard
+            let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+            let window = windowScene.windows.first
+        else { return }
+        
+        let onboardingViewController = OnboardingViewController()
+        window.rootViewController = onboardingViewController
+        window.makeKeyAndVisible()
     }
 }
