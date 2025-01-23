3242d7e43e241ebc48d3ce9cc2dbcd4fa7ddab41
Minsoo Kang <alstn38@naver.com>
2025-01-23 17:12:01 +0900

    feat: Onboarding ViewController 뷰 속성 설정

--- MSValueTransfer/MSValueTransfer/Present/OnboardingViewController.swift
+++ MSValueTransfer/MSValueTransfer/Present/OnboardingViewController.swift
@@ -5,11 +5,43 @@
 //  Created by 강민수 on 1/23/25.
 //
 
+import SnapKit
 import UIKit
 
-class OnboardingViewController: UIViewController {
+final class OnboardingViewController: UIViewController {
+    
+    private let startButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("시작하기", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.titleLabel?.font = .systemFont(ofSize: 34, weight: .bold)
+        button.layer.borderWidth = 2
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.cornerRadius = 20
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        configureView()
+        configureHierarchy()
+        configureLayout()
+    }
+    
+    private func configureView() {
+        view.backgroundColor = .white
+    }
+    
+    private func configureHierarchy() {
+        view.addSubview(startButton)
+    }
+    
+    private func configureLayout() {
+        startButton.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.height.equalTo(60)
+            $0.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(40)
+        }
     }
 }
