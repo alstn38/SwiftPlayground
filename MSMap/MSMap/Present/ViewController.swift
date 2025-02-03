b2a3e8d6b48b314c0fe61f59839d90b34761b709
Minsoo Kang <alstn38@naver.com>
2025-02-03 16:40:35 +0900

    init: MSMap project 생성

---
+++ MSMap/MSMap/Present/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MSMap
+//
+//  Created by 강민수 on 2/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
