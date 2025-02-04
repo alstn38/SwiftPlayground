7108d58343c9eac43185ddfe6ea4750039a0c274
Minsoo Kang <alstn38@naver.com>
2025-02-04 15:40:56 +0900

    feat: UICollectionVIewCell+Extension identifier 추가

---
+++ MSMap/MSMap/Extension/UICollectionViewCell+.swift
@@ -0,0 +1,15 @@
+//
+//  UICollectionViewCell+.swift
+//  MSMap
+//
+//  Created by 강민수 on 2/4/25.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
