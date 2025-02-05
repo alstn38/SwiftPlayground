b9a4564418fa632a13cb452704f15a7ed7f9227c
Minsoo Kang <alstn38@naver.com>
2025-02-05 16:18:16 +0900

    init: MSObservable project 생성

---
+++ MSObservable/MSObservable/App/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  MSObservable
+//
+//  Created by 강민수 on 2/5/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ MSObservable/MSObservable/Present/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  MSObservable
+//
+//  Created by 강민수 on 2/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .cyan
+    }
+}
