1e07e93e2a40fcede39db1aa71020043407af428
Minsoo Kang <alstn38@naver.com>
2025-02-11 18:12:03 +0900

    chore: NasaViewController 의존성 주입

--- MSUniverse/MSUniverse/SceneDelegate.swift
+++ MSUniverse/MSUniverse/SceneDelegate.swift
@@ -13,7 +13,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: NasaViewController())
+        
+        let nasaViewModel = NasaViewModel()
+        let nasaViewController = NasaViewController(viewModel: nasaViewModel)
+        window?.rootViewController = UINavigationController(rootViewController: nasaViewController)
         window?.makeKeyAndVisible()
     }
 }
