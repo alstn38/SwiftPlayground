ac5b29362a830e341d1d6203d3eb4bf206f18461
Minsoo Kang <alstn38@naver.com>
2025-02-11 16:23:31 +0900

    init: MSUniverse project 생성

---
+++ MSUniverse/MSUniverse/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MSUniverse
+//
+//  Created by 강민수 on 2/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
