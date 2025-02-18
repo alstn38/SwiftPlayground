f99b2dc587c1d39277a7ec1b87d3d9aa3785ba53
Minsoo Kang <alstn38@naver.com>
2025-02-18 16:27:02 +0900

    init: MSRxSwift project 생성

---
+++ MSRxSwift/MSRxSwift/App/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  MSRxSwift
+//
+//  Created by 강민수 on 2/18/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let viewController = ViewController()
+        let mainNavigationController = UINavigationController(rootViewController: viewController)
+        window = UIWindow(windowScene: scene)
+        window?.rootViewController = mainNavigationController
+        window?.makeKeyAndVisible()
+    }
+}
---
+++ MSRxSwift/MSRxSwift/Present/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MSRxSwift
+//
+//  Created by 강민수 on 2/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
