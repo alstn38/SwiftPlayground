556ef248395b0bcf912f8df9298c7ffa219db898
Minsoo Kang <alstn38@naver.com>
2025-02-19 18:45:16 +0900

    feat: InputOutputModel interface 정의

---
+++ MSRxSwift/MSRxSwift/Protocol/InputOutputModel.swift
@@ -0,0 +1,15 @@
+//
+//  InputOutputModel.swift
+//  MSRxSwift
+//
+//  Created by 강민수 on 2/19/25.
+//
+
+import Foundation
+
+protocol InputOutputModel {
+    associatedtype Input
+    associatedtype Output
+    
+    func transform(from: Input) -> Output
+}
