322bb616769de841ffd54548d137326cb7f96e98
Minsoo Kang <alstn38@naver.com>
2025-02-25 20:53:17 +0900

    feat: NetworkManager Single Result method 추가

--- MSShopping/MSShopping/Network/NetworkManager.swift
+++ MSShopping/MSShopping/Network/NetworkManager.swift
@@ -5,23 +5,49 @@
 //  Created by 강민수 on 1/16/25.
 //
 
-import Alamofire
 import Foundation
-
-enum NaverNetworkError: Error {
-    case invalidRequest // 400
-    case systemError // 500
-    case decodingError
-    case noPermission // 403
-    case invalidAPI // 404
-    case unknown(String)
-}
+import Alamofire
+import RxSwift
 
 final class NetworkManager {
     
     static let shared = NetworkManager()
     
     private init() { }
+    
+    func getShoppingResult(searchedText: String, sortType: String, page: Int) -> Single<Result<Product, NaverNetworkError>> {
+        
+        return Single<Result<Product, NaverNetworkError>>.create { observer in
+            let url = Bundle.main.mainURL + "?query=\(searchedText)&display=100&sort=\(sortType)&start=\(page)"
+            let header: HTTPHeaders = [
+                "X-Naver-Client-Id": Bundle.main.clientID,
+                "X-Naver-Client-Secret": Bundle.main.apiKey
+            ]
+            
+            AF.request(url, method: .get, headers: header)
+                .validate(statusCode: 200...299)
+                .responseDecodable(of: Product.self) { response in
+                switch response.result {
+                case .success(let value):
+                    observer(.success(.success(value)))
+                    
+                case .failure(let error):
+                    if error.underlyingError is DecodingError {
+                        return observer(.success(.failure(.decodingError)))
+                    }
+                    
+                    switch response.response?.statusCode {
+                    case 400: return observer(.success(.failure(.invalidRequest)))
+                    case 403: return observer(.success(.failure(.noPermission)))
+                    case 404: return observer(.success(.failure(.invalidRequest)))
+                    default: return observer(.success(.failure(.unknown(error.localizedDescription))))
+                    }
+                }
+            }
+            
+            return Disposables.create()
+        }
+    }
     
     func getShoppingResult(searchedText: String, sortType: String, page: Int, completionHandler: @escaping (Result<Product, NaverNetworkError>) -> Void) {
         guard let encodeString = searchedText.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
