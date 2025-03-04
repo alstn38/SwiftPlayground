14e1ac49b0f3be20b15b8a034910f09cbffad906
Minsoo Kang <alstn38@naver.com>
2025-03-04 18:53:25 +0900

    feat: ProductEntity Model 정의

---
+++ MSShopping/MSShopping/Present/SearchResult/Model/ProductEntity.swift
@@ -0,0 +1,18 @@
+//
+//  ProductEntity.swift
+//  MSShopping
+//
+//  Created by 강민수 on 3/4/25.
+//
+
+import Foundation
+
+struct ProductEntity {
+    let image: String
+    let mallName: String
+    let title: String
+    let link: String
+    let lowPrice: String
+    let productId: String
+    var favorite: Bool
+}
