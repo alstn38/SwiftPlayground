8a183f4cddfa0de9e5035157b7effe67f2a7403c
Minsoo Kang <alstn38@naver.com>
2025-03-05 18:39:33 +0900

    feat: WishListViewModel 정의 및 기능 구현

---
+++ MSShopping/MSShopping/Present/WishList/ViewModel/WishListViewModel.swift
@@ -0,0 +1,62 @@
+//
+//  WishListViewModel.swift
+//  MSShopping
+//
+//  Created by 강민수 on 3/5/25.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+final class WishListViewModel {
+    
+    struct Input {
+        let addWishList: Observable<String>
+    }
+    
+    struct Output {
+        let folderName: Driver<String>
+        let updateWishList: Driver<[WishList]>
+        let alertError: Driver<(title: String, message: String)>
+    }
+    
+    private let folder: WishCategory
+    private let wishListRepository: WishListRepository
+    private let disposeBag = DisposeBag()
+    
+    init(folder: WishCategory, wishListRepository: WishListRepository = DefaultWishListRepository()) {
+        self.folder = folder
+        self.wishListRepository = wishListRepository
+    }
+    
+    func transform(from input: Input) -> Output {
+        let folderNameRelay = BehaviorRelay(value: folder.folderName)
+        let updateWishListRelay: BehaviorRelay<[WishList]> = BehaviorRelay(value: Array(folder.detail))
+        let alertErrorRelay = PublishRelay<(title: String, message: String)>()
+        
+        input.addWishList
+            .bind(with: self) { owner, wishListName in
+                do {
+                    try owner.wishListRepository.createItem(folder: owner.folder, name: wishListName)
+                    updateWishListRelay.accept(Array(owner.folder.detail))
+                } catch {
+                    alertErrorRelay.accept((title: "로컬 데이터 오류", message: error.localizedDescription))
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(
+            folderName: folderNameRelay.asDriver(),
+            updateWishList: updateWishListRelay.asDriver(),
+            alertError: alertErrorRelay.asDriver(onErrorJustReturn: (title: "", message: ""))
+        )
+    }
+}
+
+extension WishListViewModel {
+    
+    enum Section: CaseIterable {
+        case product
+    }
+}
