1b5027cca1ba4856b6ba9dbab410c4754403f21d
Minsoo Kang <alstn38@naver.com>
2025-03-05 16:15:26 +0900

    feat: WishLIst Model 정의

---
+++ MSShopping/MSShopping/Model/WishList.swift
@@ -0,0 +1,24 @@
+//
+//  WishList.swift
+//  MSShopping
+//
+//  Created by 강민수 on 3/5/25.
+//
+
+import Foundation
+import RealmSwift
+
+final class WishList: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var productName: String
+    @Persisted var date: Date
+    
+    @Persisted(originProperty: "detail")
+    var folder: LinkingObjects<WishCategory>
+    
+    convenience init(productName: String, date: Date) {
+        self.init()
+        self.productName = productName
+        self.date = Date()
+    }
+}
