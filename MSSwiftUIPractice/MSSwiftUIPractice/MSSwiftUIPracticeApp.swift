b3ba83a0720f3059664cbc2332ede4ef0450ee9b
Minsoo Kang <alstn38@naver.com>
2025-04-14 19:04:02 +0900

    init: MSSwiftUIPractice project 생성

---
+++ MSSwiftUIPractice/MSSwiftUIPractice/MSSwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MSSwiftUIPracticeApp.swift
+//  MSSwiftUIPractice
+//
+//  Created by 강민수 on 4/14/25.
+//
+
+import SwiftUI
+
+@main
+struct MSSwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
