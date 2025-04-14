37b40e6a6bfc56f045db744a7a9f50d5d79ea59f
Minsoo Kang <alstn38@naver.com>
2025-04-14 20:54:20 +0900

    feat: HomeItemView UI 구현

---
+++ MSSwiftUIPractice/MSSwiftUIPractice/Home/Sub/HomeItemView.swift
@@ -0,0 +1,27 @@
+//
+//  HomeItemView.swift
+//  MSSwiftUIPractice
+//
+//  Created by 강민수 on 4/14/25.
+//
+
+import SwiftUI
+
+struct HomeItemView: View {
+    let homeMenuItem: HomeMenuItem
+    
+    var body: some View {
+        HStack {
+            Image(systemName: homeMenuItem.systemImage)
+                .foregroundColor(.white)
+            
+            Text(homeMenuItem.title)
+                .foregroundColor(.white)
+            
+            Spacer()
+            
+            Image(systemName: "chevron.right")
+                .foregroundColor(.gray)
+        }
+    }
+}
