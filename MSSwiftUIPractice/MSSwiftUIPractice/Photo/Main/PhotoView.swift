3b8719f7d57873c21574661f266a84ae1270bf93
Minsoo Kang <alstn38@naver.com>
2025-04-15 16:55:48 +0900

    feat: PhotoView UI 작업

---
+++ MSSwiftUIPractice/MSSwiftUIPractice/Photo/Main/PhotoView.swift
@@ -0,0 +1,31 @@
+//
+//  PhotoView.swift
+//  MSSwiftUIPractice
+//
+//  Created by 강민수 on 4/14/25.
+//
+
+import SwiftUI
+
+struct PhotoView: View {
+    
+    var body: some View {
+        
+        NavigationView {
+            ScrollView(.vertical) {
+                LazyVStack {
+                    ForEach(PhotoSectionItem.sections, id: \.id) { section in
+                        PhotoSectionView(section: section)
+                    }
+                }
+            }
+            .scrollIndicators(.hidden)
+            .navigationTitle("My Random Image")
+            .navigationBarTitleDisplayMode(.large)
+        }
+    }
+}
+
+#Preview {
+    PhotoView()
+}
